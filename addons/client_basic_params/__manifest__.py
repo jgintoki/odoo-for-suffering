0118e6e572897e19f4a1ec09cb9582cbd803c1c1
Junior C <junior.corales@externo.amicar.cl>
2025-08-13 15:48:53 -0400

    update version

--- addons/client_basic_params/__manifest__.py
+++ addons/client_basic_params/__manifest__.py
@@ -1,7 +1,7 @@
 # __manifest__.py
 {
     'name': 'Client Basic Parameters',
-    'version': '1.6.9',
+    'version': '1.6.10',
     'summary': 'Basic parameters for client management',
     'sequence': 10,
     'description': """
